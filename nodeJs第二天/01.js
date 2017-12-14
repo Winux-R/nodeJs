5e39691c843656dfc6bd808b71bd9644ee1b2bd4
Winnx-R <464940202@qq.com>
2017-12-14 16:47:34 +0800

    nodejs学习编码 http fs path url

--- nodeJs第二天/01.js
+++ nodeJs第二天/01.js
@@ -1,3 +1,11 @@
-/**
+ /**
  * Created by Winux-R on 2017/12/14.
  */
+const http = require("http");
+
+http.createServer( (req, res) => {
+    res.writeHead(200, {"Content-type": "text/html;charset=UTF-8"});
+    res.end("成功");
+}).listen(3000, () => {
+    console.log("server start");
+})
